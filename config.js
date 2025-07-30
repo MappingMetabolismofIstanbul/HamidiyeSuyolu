46730b85bc481493b21543d1c87a8f125ace7a4c
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-30 11:50:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Halkalı Suyolu / Halkalı Waterway',
+    title: 'Hamidiye Suyolu / Hamidiye Waterway',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
