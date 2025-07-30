59f9cfad986ea04c4e38ee963cd7f6b9a9cfc0c6
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-30 11:45:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Hamidiye Suyolu / Hamidiye Waterway',
+    title: 'İstanbul'da Suyun Metabolik Akışı: Hamidiye Suyolu / Metabolic Flows of İstanbul: Hamidiye Waterway',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
